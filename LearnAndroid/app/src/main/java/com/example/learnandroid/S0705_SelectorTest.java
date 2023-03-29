87d5ce37ffb64f97267cccd7ebe4c674afacebd9
hansrobothans <hansrobothans@gmail.com>
2023-03-29 20:37:45 +0800

    selector的使用
    07 项目6：校友名录—高级控件
      05 selector的使用
    slector和shape的使用

---
+++ LearnAndroid/app/src/main/java/com/example/learnandroid/S0705_SelectorTest.java
@@ -0,0 +1,14 @@
+package com.example.learnandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class S0705_SelectorTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.s0705_selector_test);
+    }
+}
