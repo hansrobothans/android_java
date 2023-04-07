987c9f60b2d2a8183e1d1d690c5540e8f5d9274d
hansrobothans <hansrobothans@gmail.com>
2023-04-07 20:51:19 +0800

    MyFragment  MainActivity改名为ViewPagerActivity

--- MyFragment/app/src/main/java/com/example/myfragment/ViewPagerActivity.java
+++ MyFragment/app/src/main/java/com/example/myfragment/ViewPagerActivity.java
@@ -1,20 +1,19 @@
 package com.example.myfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 import com.example.myfragment.Adapter.ViewPagerAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class ViewPagerActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_view_pager_main);
 
 //        1. 新建布局对象并绑定
         ViewPager2 viewPager = findViewById(R.id.viewPager);
