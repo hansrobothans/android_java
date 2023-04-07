da95358294e52300107b350557fb1b3aa27bcb95
hansrobothans <hansrobothans@gmail.com>
2023-04-07 09:34:05 +0800

    mooc  S040302_ActivityDataBack

---
+++ mooc/app/src/main/java/com/example/mooc/S040302_ActivityDataBack2.java
@@ -0,0 +1,30 @@
+package com.example.mooc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class S040302_ActivityDataBack2 extends AppCompatActivity {
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.s040302_data_back2);
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("ExtraData", "从第二个界面来的数据");
+                //设置返回结果，将“ExtraData”的值 通过intent返回
+                setResult(1, intent);
+                finish();
+            }
+        });
+    }
+}
