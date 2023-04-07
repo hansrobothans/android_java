0a9a9ad473b30f8969e2465b6d5cc017dd84e8dc
hansrobothans <hansrobothans@gmail.com>
2023-04-07 10:42:09 +0800

    mooc  S0705_SelectorTest  注意主题改变

---
+++ mooc/app/src/main/java/com/example/mooc/S0705_SelectorTest.java
@@ -0,0 +1,14 @@
+package com.example.mooc;
+//如果运行现象不正常，核对主题
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class S0705_SelectorTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.s0705_selector_test);
+    }
+}
