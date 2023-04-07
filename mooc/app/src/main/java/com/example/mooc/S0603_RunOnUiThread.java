250d3351c9e4b08ceff105f06c4a28b672302c3f
hansrobothans <hansrobothans@gmail.com>
2023-04-07 09:46:22 +0800

    mooc  S0603_RunOnUiThread

--- mooc/app/src/main/java/com/example/mooc/S0603_RunOnUiThread.java
+++ mooc/app/src/main/java/com/example/mooc/S0603_RunOnUiThread.java
@@ -1,14 +1,13 @@
-package com.example.learnandroid;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.mooc;
 
 import android.os.Bundle;
-import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class S0603_RunOnUiThread extends AppCompatActivity {
 //    定义用到的三个控件
