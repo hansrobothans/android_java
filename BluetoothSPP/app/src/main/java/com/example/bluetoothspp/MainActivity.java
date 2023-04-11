86cab9b029bf4b607ebbce31b8f9cc7c1a874327
hansrobothans <hansrobothans@gmail.com>
2023-04-11 10:51:21 +0800

    BluetoothSPP   V1.1.2   初步构建listview

--- BluetoothSPP/app/src/main/java/com/example/bluetoothspp/MainActivity.java
+++ BluetoothSPP/app/src/main/java/com/example/bluetoothspp/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.bluetoothspp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.bluetoothspp.adapter.AdapterBluetoothSPPScan;
+import com.example.bluetoothspp.entity.ItemBluetoothSPPScan;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+//    声明一个ItemBluetoothSPPScan类型的数组
+    private List<ItemBluetoothSPPScan> mData = null;
+    private AdapterBluetoothSPPScan mAdapter = null;
+//    声明ListView控件
+    private ListView listView;
+//    上下文
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        上下文赋值
+        mContext = this;
+//        绑定控件
+        listView = (ListView) findViewById(R.id.listview);
+
+//        手动生成数据
+        mData = new ArrayList<ItemBluetoothSPPScan>();
+        for (int i = 0; i < 20; i++) {
+            mData.add(new ItemBluetoothSPPScan("Name" + i, "Address" + i));
+        }
+
+//        新建adapter
+        mAdapter = new AdapterBluetoothSPPScan(mData, mContext);
+//        设置adapter
+        listView.setAdapter(mAdapter);
+//        设置点击事件
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(mContext, "点击了第" + position + "条数据", Toast.LENGTH_SHORT).show();
+            }
+        });
+//        设置长按事件
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(mContext, "长按了第" + position + "条数据", Toast.LENGTH_SHORT).show();
+
+//                长按被触发，触发后item长按状态 自动 消失，并且不会触发点击。
+//                return true;
+//                长按被触发，松手后点击被触发。且不松手一直处于长按状态。
+                return false;
+            }
+        });
     }
 }
---
+++ BluetoothSPP/app/src/main/java/com/example/bluetoothspp/entity/ItemBluetoothSPPScan.java
@@ -0,0 +1,31 @@
+package com.example.bluetoothspp.entity;
+
+public class ItemBluetoothSPPScan {
+    private String name;
+    private String address;
+
+    public ItemBluetoothSPPScan() {
+    }
+
+    public ItemBluetoothSPPScan(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
